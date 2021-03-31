2ca9813221be96cf6c6447889806e1d5fa839d0c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-30 19:44:37 -0700

    Detect_V1.php
    An archive of the first version of the anti-AdBlock detection PHP script.

---
+++ AdBool/WHATWG/PHP/Detect/OldVersions/1/Detect_V1.php
@@ -0,0 +1,14 @@
+<?php
+// Start of script
+// This script is for detecting anti-adblock scripts
+// This script is under development and is currently just a demo. I am not very good at writing in PHP at the moment
+class detector() {
+  $detectA = "/";
+}
+/* File info
+* File version: 1 (Tuesday, March 30th 2021 at 7:44 pm)
+* File type: PHP 8.0.3 Source file (*.php)
+* Line count (including blank lines and compiler line): 15
+*/
+// End of script
+?>
