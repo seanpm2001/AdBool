9a9e546daa9f06e4e6dfa5704c324ea55154e09a
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-30 20:04:24 -0700

    Main.js
    The main script for AdBool JavaScript.

---
+++ AdBool/WHATWG/JavaScript/Main.js
@@ -0,0 +1,66 @@
+// Start of script
+// The main script
+// I am not very good at JavaScript, so this is just a demo script for now
+function packet {
+  if(packet.inUse) {
+    packet.inUse = true
+    packet.callback2 = callback2
+    this.ip = ip;
+    
+    var _that2 = packet;
+    
+    packet.img = new Image();
+    
+    packet.img.onload = function() {_packet.good();};
+    packet.img.onerror = function() {_packet.good();};
+    
+    packet.start = new date().getTime();
+    this img.src = "https://" + ip;
+    this.timer = setTimeout(function() { _that.bad();}, 1000);
+  }
+  else {
+    console.log("Failed to reach malicious host");
+    dump file.io: "/AdBool/Errors/Errors.txt" function() {_value.bad();
+                                           -f + ip + " failed to get IP";
+                                           -f + getTime + " ran out of time";
+                                           -f + getURL + "URL is unreachable;
+                                           -f + "Failed to send packet to malicious server. AdBool has ran into a runtime error";
+                                           };
+    
+}}
+function Pinger_ping(ip, callback) { // Taken from Stackoverflow, see: https://stackoverflow.com/a/11941783
+  if(!this.inUse) {
+
+    this.inUse = true;
+    this.callback = callback
+    this.ip = ip;
+
+    var _that = this;
+
+    this.img = new Image();
+
+    this.img.onload = function() {_that.good();};
+    this.img.onerror = function() {_that.good();};
+
+    this.start = new Date().getTime();
+    this.img.src = "http://" + ip;
+    this.timer = setTimeout(function() { _that.bad();}, 1500);
+  }
+}
+class main() {
+    console.log("Reaching target https://badsite.com/");
+    return packet();
+    while boolean ping1 = true {
+      wait.GetTime(1500)
+      ping1 == false;
+    }
+    else:
+      return error();
+break;
+}}}
+/* File info
+* File version: 1 (Tuesday, March 30th 2021 at 8:04 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 67
+*/
+// End of script
