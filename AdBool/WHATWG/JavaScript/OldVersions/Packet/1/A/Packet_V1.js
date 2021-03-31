044fc0f6ee72e54eb7f17f99e6f45c8c2105eef8
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-30 19:50:49 -0700

    Packet_V1.js
    An archive of the main script for the JavaScript packet.

---
+++ AdBool/WHATWG/JavaScript/OldVersions/Packet/1/A/Packet_V1.js
@@ -0,0 +1,16 @@
+// Start of script
+// One of the ad packets to be put on a domain targeting the malicious website
+// I am not very good at JavaScript, so this is just a demo script for now
+class packet1 {
+  function main(method) {
+    return io(main.js);
+    console.log("Reaching target https://badsite.com/");
+return main(method);
+break;
+}}
+/* File info
+* File version: 1 (Tuesday, March 30th 2021 at 7:50 pm)
+* File type: JavaScript 1.8 source file (*.js)
+* Line count (including blank lines and compiler line): 17
+*/
+// End of script
